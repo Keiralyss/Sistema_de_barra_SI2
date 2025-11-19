e11765542181f7688e764fc19a4d98ec68435fc1
Geraldine Martínez <gemarcid@gmail.com>
2025-11-19 15:51:06 -0300

    nologo

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,15 +19,8 @@ function HomePage({ count }) {
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
+
+            <h1>Escanea tu credencial</h1>
 
             <div className="card">
                 <button onClick={handleNavigation}>
